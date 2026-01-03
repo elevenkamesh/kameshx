0b1b194adf5f23d65cbc4e90d70861cfa33a3113
elevenkamesh <11kameshwaran@gmail.com>
2026-01-03 19:25:58 +0530

    added java

--- src/componnets/skills.jsx
+++ src/componnets/skills.jsx
@@ -1,116 +1,128 @@
-import { useTranslation } from 'react-i18next';
-import GetImage from '../utils/images.jsx';
+import { useTranslation } from "react-i18next";
+import GetImage from "../utils/images.jsx";
 
 const images = GetImage();
 
 const Skills = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const skills = [
-        {
-            image: images.js,
-            title: 'JavaScript',
-            description: 'programing language'
-        },
-        {
-            image: images.ts,
-            title: 'TypeScript',
-            description: 'wrapper of javaScript'
-        },
-        {
-            image: images.python,
-            title: 'Python',
-            description: 'programing language'
-        },
-        {
-            image: images.golang,
-            title: 'Golang',
-            description: 'programing language'
-        },
-        {
-            image: images.react,
-            title: 'React',
-            description: 'frontend library'
-        },
-        {
-            image: images.node,
-            title: 'Node Js',
-            description: 'JS runtime'
-        },
-        
-        {
-            image: images.FastAPI,                                                                                            
-            title: 'FastAPI',
-            description: 'backend framework'
-        },
-        {
-            image: images.mongo,
-            title: 'MongoDB',
-            description: 'Database'
-        },
-        {
-            image: images.posgress,
-            title: 'PostgreSQL',
-            description: 'Database'
-        },
-        {
-            image: images.aws,
-            title: 'AWS',
-            description: 'cloud Provider'
-        },
-        {
-            image: images.git,
-            title: 'Git',
-            description: 'version control'
-        },
-        {
-            image: images.docker,
-            title: 'Docker',
-            description: 'containerization'
-        },
-        // {
-        //     image: images.kubernetes,
-        //     title: 'K8s',
-        //     description: 'orchestration'
-        // },
-        {
-            image: images.bash,
-            title: 'Bash',
-            description: 'scripting'
-        }
-    ];
+  const skills = [
+    {
+      image: images.js,
+      title: "JavaScript",
+      description: "programing language",
+    },
+    {
+      image: images.ts,
+      title: "TypeScript",
+      description: "wrapper of javaScript",
+    },
+    {
+      image: images.java,
+      title: "Java",
+      description: "programing language",
+    },
 
-    return (
-          <section id="skills">
-        <div className="container py-5">
-            <h1 className="section-title text-center mb-5">{t("heading.skills")}</h1>
+    {
+      image: images.python,
+      title: "Python",
+      description: "programing language",
+    },
+    {
+      image: images.golang,
+      title: "Golang",
+      description: "programing language",
+    },
+    {
+      image: images.spring,
+      title: "Spring Boot",
+      description: "Java framework",
+    },
+    {
+      image: images.react,
+      title: "React",
+      description: "frontend library",
+    },
+    {
+      image: images.node,
+      title: "Node Js",
+      description: "JS runtime",
+    },
 
-            <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 g-4">
-                {skills.map((skill, index) => (
-                    <div className="col" key={index}>
-                        <div className="skill-card h-100 d-flex flex-column align-items-center p-3 text-center">
-                            <img 
-                                src={skill.image} 
-                                alt={skill.title}
-                                className="skill-icon mb-3" 
-                                width="60" 
-                                height="60"
-                                loading="lazy"
-                            />
-                            <h3 className="skill-title mb-1">{skill.title}</h3>
-                            {/* {skill.description && (
+    {
+      image: images.FastAPI,
+      title: "FastAPI",
+      description: "backend framework",
+    },
+    {
+      image: images.mongo,
+      title: "MongoDB",
+      description: "Database",
+    },
+    {
+      image: images.posgress,
+      title: "PostgreSQL",
+      description: "Database",
+    },
+    {
+      image: images.aws,
+      title: "AWS",
+      description: "cloud Provider",
+    },
+    {
+      image: images.git,
+      title: "Git",
+      description: "version control",
+    },
+    {
+      image: images.docker,
+      title: "Docker",
+      description: "containerization",
+    },
+    // {
+    //     image: images.kubernetes,
+    //     title: 'K8s',
+    //     description: 'orchestration'
+    // },
+    {
+      image: images.bash,
+      title: "Bash",
+      description: "scripting",
+    },
+  ];
+
+  return (
+    <section id="skills">
+      <div className="container py-5">
+        <h1 className="section-title text-center mb-5">
+          {t("heading.skills")}
+        </h1>
+
+        <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 g-4">
+          {skills.map((skill, index) => (
+            <div className="col" key={index}>
+              <div className="skill-card h-100 d-flex flex-column align-items-center p-3 text-center">
+                <img
+                  src={skill.image}
+                  alt={skill.title}
+                  className="skill-icon mb-3"
+                  width="60"
+                  height="60"
+                  loading="lazy"
+                />
+                <h3 className="skill-title mb-1">{skill.title}</h3>
+                {/* {skill.description && (
                                 <p className="skill-description small text-muted mb-0">
                                     {skill.description}
                                 </p>
                             )} */}
-                        </div>
-                    </div>
-                ))}
+              </div>
             </div>
+          ))}
         </div>
-
-        </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
--- src/utils/images.jsx
+++ src/utils/images.jsx
@@ -1,26 +1,24 @@
-import kubernetes from '../assets/tech/kubernetes.svg'
-import aws from '../assets/tech/aws.svg'
-import git from '../assets/tech/git.svg'
-import docker from '../assets/tech/docker.svg'
-import golang from '../assets/tech/go.svg'
-import tailwind from '../assets/tech/tailwindcss.svg'
-import js from '../assets/tech/javascript-js.svg'
-import react from '../assets/tech/react.svg'
-import ts from '../assets/tech/typescript-icon.svg'
-import node from '../assets/tech/node.png'
-import mongo from '../assets/tech/mongodb.svg'
-import posgress from '../assets/tech/postgresql.svg'
-import python from '../assets/tech/python.svg'
-import bash from '../assets/tech/bash.svg'
-import FastAPI from '../assets/tech/FastAPI.svg'
-
-
-
-
-
-
-   const GetImage = () =>{
-return {
+import kubernetes from "../assets/tech/kubernetes.svg";
+import aws from "../assets/tech/aws.svg";
+import git from "../assets/tech/git.svg";
+import docker from "../assets/tech/docker.svg";
+import golang from "../assets/tech/go.svg";
+import tailwind from "../assets/tech/tailwindcss.svg";
+import js from "../assets/tech/javascript-js.svg";
+import react from "../assets/tech/react.svg";
+import ts from "../assets/tech/typescript-icon.svg";
+import node from "../assets/tech/node.png";
+import mongo from "../assets/tech/mongodb.svg";
+import posgress from "../assets/tech/postgresql.svg";
+import python from "../assets/tech/python.svg";
+import bash from "../assets/tech/bash.svg";
+import FastAPI from "../assets/tech/FastAPI.svg";
+import spring from "../assets/tech/spring.svg";
+import java from "../assets/tech/java.svg";
+const GetImage = () => {
+  return {
+    java,
+    spring,
     kubernetes,
     aws,
     git,
@@ -35,9 +33,8 @@ return {
     mongo,
     python,
     bash,
-    FastAPI
-
-}
-}
+    FastAPI,
+  };
+};
 
 export default GetImage;
